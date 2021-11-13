936adf3204892e4d50e234a030b8f03a635b563b
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-11-13 16:30:38 +0600

    update

--- index.js
+++ index.js
@@ -94,6 +94,19 @@ async function run() {
         res.send(result);
       });
 
+       
+    // GET USER
+      app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let isAdmin = false;
+        if (user?.role === 'admin') {
+            isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+    })
+
     // POST User 
       app.post("/users", async (req, res) => {
         const user = req.body;
